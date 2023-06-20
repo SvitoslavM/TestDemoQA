5e1f139be49aadb118ad72cc0c029e042496c016
mba <greenenergy15@gmail.com>
2023-06-20 15:29:16 +0300

    Add allure framework and add step for test

--- tests/test_form.py
+++ tests/test_form.py
@@ -1,11 +1,14 @@
-import time
 from pages.main_page import MainPage
+import allure
 
 
+@allure.story('Open main page')
 def test_practice_form(driver):
-    page = MainPage(driver)
+    with allure.step('Open main page'):
+        page = MainPage(driver)
     page.open()
-    click_form_main_page = page.open_form_main_page()
+    with allure.step('click on form page, then on form page click on practice rorm '):
+        click_form_main_page = page.open_form_main_page()
     open_practice_form = click_form_main_page.open_form_page()
-    assert open_practice_form.get_text_form_page().text == "Student Registration Form", 'ERROR'
-
+    with allure.step("go to test practice form text"):
+        assert open_practice_form.get_text_form_page().text == "Student Registration Form", 'ERROR'
