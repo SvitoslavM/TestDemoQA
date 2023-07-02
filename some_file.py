daf6b4a16d28a36ec87456cee593a21d9363e2b7
Sviatoslav M <73024226+SvitoslavM@users.noreply.github.com>
2023-07-02 11:26:58 +0300

    Delete some_file.py

--- some_file.py
+++
@@ -1,9 +0,0 @@
-data1 = ['commands', 'react', 'private', 'classified', 'general', 'downloads', 'wordFile', 'excelFile']
-
-data2 = ['Commands', 'React', 'Private', 'Classified', 'General', 'Downloads', 'Word File.doc', 'Excel File.doc']
-
-data2 = str(data2).replace(' ', '').replace('doc', '').replace('.', '').lower()
-data1 = str(data1).lower()
-data1 = str(data1).replace(" ", "").lower()
-"""How to change text if we have different results"""
-assert data2 == data1
